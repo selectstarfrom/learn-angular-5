f1a767afcbe9e291592f0696e019bba670193455
iamfroz <this4coding@gmail.com>
2017-11-30 22:29:12 +0400

    recipe-corner done

---
+++ recipe-corner/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [];
+
+  constructor() {
+    this.ingredients.push(
+      new Ingredient('Apple', 2),
+      new Ingredient('Lemonade', 4)
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
