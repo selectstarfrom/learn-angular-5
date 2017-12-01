f0eab7f06b27c68b15f9825a00b5102bb8aeb3aa
iamfroz <this4coding@gmail.com>
2017-12-01 09:53:01 +0400

    feature switch

--- recipe-corner/src/app/header/header.component.ts
+++ recipe-corner/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component(
     {
@@ -8,4 +8,10 @@ import { Component } from "@angular/core";
 )
 export class HeaderComponent {
 
+    @Output()
+    featureSelect: EventEmitter<string> = new EventEmitter<string>();
+
+    onSelect(feature: string) {
+        this.featureSelect.emit(feature);
+    }
 }
