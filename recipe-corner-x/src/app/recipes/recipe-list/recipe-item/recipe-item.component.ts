a9c54fd0a2e700befe7e732d655b0df57bccba9a
selectstarfrom <this4coding@gmail.com>
2017-12-04 10:32:36 +0400

    recipe-item routing

--- recipe-corner-x/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-corner-x/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,7 +9,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input('recipe') recipe: Recipe;
+  @Input() recipe: Recipe;
+  @Input() index: number;
 
   constructor(private recipeService: RecipeService) { }
 
